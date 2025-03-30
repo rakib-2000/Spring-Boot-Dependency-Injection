e8348b7fcfd6ac365c36b292958d42bd8f17d406
rakx07 <rakib15-13846@diu.edu.bd>
2025-03-30 12:28:09 +0600

    Some comments are added for better understanding of the topic.

--- src/main/java/com/rakx07/configuration/BeanConfiguration.java
+++ src/main/java/com/rakx07/configuration/BeanConfiguration.java
@@ -6,9 +6,25 @@ import com.rakx07.model.Supplier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+/*
+Bean class which controls the object creation.
+In this case it'll first create supplier object and pass it to Computer method.
+Computer method will take supplier object and create computer object and pass it to Student method.
+Student method will take computer object and create student object.
+ */
+
+/**
+This is inversion of control method. Means we're giving the access of creating object to another
+ class which will automate the object creation process
+ */
+@Configuration // Makes it a configuration class and makes it available when the program executes.
 public class BeanConfiguration {
 
+    /*
+    Bean annotation is used to make sure the below methods are used as bean/object creation
+    Bean automatically uses Singleton design pattern.
+    But with Scope annotation we can change it to "prototype" design pattern as well "@Scope("prototype")"
+     */
     @Bean
     public Student student(Computer computer) {
         Student student = new Student();
@@ -19,7 +35,7 @@ public class BeanConfiguration {
 
     /**
      * While we need multiple Bean of the same class.
-    @Bean
+    //@Bean
     public Student secondStudentBean(Computer computer) {
         Student student = new Student();
         student.setName("Another");
